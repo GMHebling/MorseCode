d29b5240a1a8206a57cbe4e7cd9fde0b7e6dc581
demiCatson <gustavo.hebling@usp.br>
2018-12-21 08:50:57 -0200

    commit inicial

---
+++ MorseCode.ino
@@ -0,0 +1,24 @@
+//MorseCode Reader
+//
+//Criado em 21/12
+
+const char* alfabeto[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+                        "....", "..", ".---", "-.-", ".-..", "--", "-.", 
+                        "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+                        "...-", ".--", "-..-", "-.--", "--.."};
+                        
+float dotTime = 0.5;
+float dashTime = 1.5;
+
+unsigned long currentTime;
+unsigned long lastTime;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
